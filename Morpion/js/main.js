a87cc0d0ce0cd5144b916baeef7f95c4887bdb6d
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-04-12 11:37:34 +0200

    first commit

---
+++ Morpion/js/main.js
@@ -0,0 +1,31 @@
+const buttons = document.getElementsByTagName('button');
+
+
+
+buttons[0].addEventListener('click', event => {
+    buttons[0].innerText = 'test';
+});
+buttons[1].addEventListener('click', event => {
+    buttons[1].innerText = 'test';
+});
+buttons[2].addEventListener('click', event => {
+    buttons[2].innerText = 'test';
+});
+buttons[3].addEventListener('click', event => {
+    buttons[3].innerText = 'test';
+});
+buttons[4].addEventListener('click', event => {
+    buttons[4].innerText = 'test';
+});
+buttons[5].addEventListener('click', event => {
+    buttons[5].innerText = 'test';
+});
+buttons[6].addEventListener('click', event => {
+    buttons[6].innerText = 'test';
+});
+buttons[7].addEventListener('click', event => {
+    buttons[7].innerText = 'test';
+});
+buttons[8].addEventListener('click', event => {
+    buttons[8].innerText = 'test';
+});
