8d7fef24303373591912b8e414e4b3da3f6bf293
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-04-14 10:00:00 +0200

    exercice 2

---
+++ Exercice2/js/main.js
@@ -0,0 +1,6 @@
+let source = "./image/leona_high_noon___lol_wallpaper_by_editsbyjadewolf_df5axfg-pre.jpg";
+
+function chargeImage() {
+    let image = document.getElementsByTagName("IMG")[0];
+    image.setAttribute("src", source);
+}
