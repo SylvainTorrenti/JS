e12db8f28b84a613fe779f4e8caf1303ef76a633
SylvainTorrenti <torrsyl@gmail.com>
2023-04-22 10:38:33 +0200

    Commentaire

--- Exercice2/js/main.js
+++ Exercice2/js/main.js
@@ -1,6 +1,8 @@
-let source = "./image/leona_high_noon___lol_wallpaper_by_editsbyjadewolf_df5axfg-pre.jpg";
+let source = "./image/leona_high_noon___lol_wallpaper_by_editsbyjadewolf_df5axfg-pre.jpg";// Création d'une variable qui stocke le chemin d'acces à l'image
+// Fonction pour changer l'image
 
 function chargeImage() {
-    let image = document.getElementsByTagName("IMG")[0];
-    image.setAttribute("src", source);
+    let image = document.getElementsByTagName("IMG")[0]; // Récupére l'element HTML "IMG". Avec getElementsByTagName ça stocke les elements "IMG" dans un tableau même si il n'y a qu'un element donc pour selectionner l'element il faut selectionner l'element dans le tableau (ici [0] signifie que, dans le tableau où sont stockés les elements "IMG", nous selectionons le 1er element (indice 0)).
+
+    image.setAttribute("src", source);// remplacement de l'attribut "source" de l'element "IMG" par la variable créée en amont. 
 }
